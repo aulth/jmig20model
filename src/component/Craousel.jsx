f1f460c076d6df5adccd2928def83cf02dc862bc
aulth <mohdusman.you@gmail.com>
2023-04-10 17:35:48 +0530

    landing page layout done

---
+++ src/component/Craousel.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Craousel = () => {
+    return (
+        <>
+            <div className="w-screen mx-auto h-screen  relative flex xl:justify-start justify-center xl:items-stretch items-center overflow-y-hidden">
+                <video id="background-video" className='absolute top-0 z-10 xl:block hidden' autoPlay loop muted poster="https://assets.codepen.io/6093409/river.jpg">
+                    <source src="/video/language.mp4" type="video/mp4" />
+                </video>
+                <video id="background-video" className='absolute top-0 z-10 xl:hidden block' autoPlay loop muted poster="https://assets.codepen.io/6093409/river.jpg">
+                    <source src="/video/language-phone.mp4" type="video/mp4" />
+                </video>
+                <div className='z-50 flex items-center flex-col text-white xl:ml-[10%] xl:mt-[15%] mt-20'>
+                    <h2 className=" md:text-[26px] text-[20px] prim-font capitalize">WELCOME TO</h2>
+                    <h3 className="font-bold md:text-[45px] text-[30px] prim-font">G20 Model - JMI</h3>
+                    <img src="https://www.g20.org/content/dam/gtwenty/banner-carousal/vasudhaiva-kutumbakam.png"  className='md:w-80 w-64' alt="theme of g20" />
+                    <div className="w-60 h-44 relative flex justify-center items-center">
+                        <img src="https://www.g20.org/content/dam/gtwenty/banner-carousal/white-glow-bg.svg" className='absolute top-0 w-full' alt="g20jmi logo" />
+                        <img src="/images/g20-logo.png" className=' w-40 z-50' alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Craousel
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+const Navbar = () => {
+    const [menuOpened, setMenuOpened] = useState(false)
+    const toggleMenu = () =>{
+        if(typeof window!=undefined){
+            let menu = document.querySelector('.menu');
+            menu.classList.toggle('hidden')
+            setMenuOpened(!menuOpened)
+        }
+    }
+    return (
+        <header className="w-screen bg-white border-b z-50 border-orange-200 py-2 xl:px-20 px-4 flex items-center fixed top-0">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className=" w-full">
+                    <a href="#">
+                        <img src="/images/g20-logo.png" className='w-32' alt="" />
+                    </a>
+                </div>
+                <nav className='xl:flex menu w-full justify-between hidden xl:static absolute top-[61px] left-0 bg-white xl:pt-0 pt-2'>
+                    <ul className='flex xl:flex-row flex-col xl:gap-5 gap-2 xl:items-center items-start xl:px-0 px-2'>
+                        <li className='shrink-0 hover:text-[#2e59c1] xl:text-sm text-lg font-[500] border-b-2 border-white duration-200 hover:border-[#2e59c1]'><a href="#">About G20</a></li>
+                        <li className='shrink-0 hover:text-[#2e59c1]  xl:text-sm text-lg font-[500] border-b-2 border-white duration-200 hover:border-[#2e59c1]'><a href="#">Media & Resources</a></li>
+                        <li className='shrink-0 hover:text-[#2e59c1]  xl:text-sm text-lg font-[500] border-b-2 border-white duration-200 hover:border-[#2e59c1]'><a href="#">Menu Item 3</a></li>
+                    </ul>
+                    <hr className='mt-1 xl:hidden' />
+                    <ul className='flex xl:flex-row flex-col xl:gap-5 gap-2 xl:items-center items-start xl:mt-0 mt-2 xl:px-0 px-2'>
+                        <li className='shrink-0 hover:text-[#2e59c1] xl:text-sm text-lg font-[500] border-b-2 border-white duration-200 hover:border-[#2e59c1]'><a href="#">Registration</a></li>
+                        <li className='shrink-0 hover:text-[#2e59c1] xl:text-sm text-lg font-[500] border-b-2 border-white duration-200 hover:border-[#2e59c1]'><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+               <button onClick={toggleMenu} className='xl:hidden block text-2xl'>{!menuOpened?<AiOutlineMenu />:<AiOutlineClose />}</button>
+            </div>
+        </header>
+
+    )
+}
+
+export default Navbar
